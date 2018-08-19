ff249f747367c656ad469f6923458ebede069965
StepanyanS <infostepanyans@gmail.com>
2018-08-19 18:27:22 +0400

    error handling

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -60,17 +60,26 @@ export class LoginComponent implements OnInit {
   onSubmit() {
     const formData = this.loginForm.value;
     this.usersService.getUserByEmail(formData.email)
-      .subscribe((user: User) => {
-          if (user && user.password === formData.password) {
-              window.localStorage.setItem('user', JSON.stringify(user));
-              this.authService.login();
-              this.router.navigate(['/dashboard/profile']);
-          } else {
-            this.showMessage({
-              text: 'The email or password you entered is incorrect, please try again',
-              type: 'danger'
-            });
-          }
-      });
+    .subscribe((user: User) => {
+
+      if (!user) {
+        this.showMessage({
+          text: 'Something went wrong',
+          type: 'danger'
+        });
+        return;
+      }
+
+      if (user && user.password === formData.password) {
+        window.localStorage.setItem('user', JSON.stringify(user));
+        this.authService.login();
+        this.router.navigate(['/dashboard/profile']);
+      } else {
+        this.showMessage({
+          text: 'The email or password you entered is incorrect, please try again',
+          type: 'danger'
+        });
+      }
+    });
   }
 }
--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 
 // import models
 import { User } from '../models/user.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
